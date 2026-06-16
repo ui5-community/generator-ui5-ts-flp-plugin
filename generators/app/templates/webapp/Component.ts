6235adeed180859ef77e56ffbfdfe074e5db756e
Mario Kernich <mario.kernich@swan.de>
2026-06-17 01:29:17 +0200

    add: backward compatibility for ui5 less than 1.120

--- generators/app/templates/webapp/Component.ts
+++ generators/app/templates/webapp/Component.ts
@@ -1,9 +1,11 @@
 import UIComponent from "sap/ui/core/UIComponent";
 import ResourceModel from "sap/ui/model/resource/ResourceModel";
 import Container from "sap/ushell/Container";
+import MessageToast from "sap/m/MessageToast";<% if (gte1_120_0) { %>
 import Extension from "sap/ushell/services/Extension";
-import { Button$PressEvent } from "sap/m/Button";
-import MessageToast from "sap/m/MessageToast";
+import { Button$PressEvent } from "sap/m/Button";<% } else { %>
+import Renderer from "sap/ushell/renderers/fiori2/Renderer";
+import ObjectPath from "sap/base/util/ObjectPath";<% } %>
 
 /**
  * @namespace <%= pluginId %>
@@ -14,10 +16,12 @@ export default class Component extends UIComponent {
     interfaces: ["sap.ui.core.IAsyncContentCreation"]
   };
 
+  <% if (gte1_120_0) { %>
+
   public async init() {
     super.init();
 
-    // load translations from messagebundle
+    // load translations from i18n model
     const resourceModel = this.getModel("i18n") as ResourceModel;
     const resourceBundle = await resourceModel.getResourceBundle();
 
@@ -36,4 +40,39 @@ export default class Component extends UIComponent {
     item.showForAllApps();
     item.showOnHome();
   }
+
+  <% } else { %>
+
+  public async init() {
+    super.init();
+
+    // load translations from i18n model
+    const resourceModel = this.getModel("i18n") as ResourceModel;
+    const resourceBundle = await resourceModel.getResourceBundle();
+
+    const renderer = await this.getRenderer();
+
+    renderer.addHeaderEndItem("sap.m.Button", {
+      icon: "sap-icon://hello-world",
+      tooltip: resourceBundle.getText("btnText"),
+      press: () => {
+        MessageToast.show("Hello World");
+      }
+    }, true, true, []);
+  }
+
+  private async getRenderer(): Promise<Renderer> {
+    const oShellContainer = ObjectPath.get("sap.ushell.Container");
+    if (!oShellContainer) {
+      throw new Error("Illegal state: shell container not available; this component must be executed in a unified shell runtime context.");
+    }
+    const oRenderer = oShellContainer.getRenderer();
+    if (oRenderer) {
+      return oRenderer;
+    } else {
+      throw new Error("Illegal state: shell renderer not available after receiving 'rendererLoaded' event.");
+    }
+  }
+
+  <% } %>
 }
